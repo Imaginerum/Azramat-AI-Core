7a4f8a952875d5d2a6cf23afb0b17adee7df14f6
Piotr <chyzy29@gmail.com>
2025-11-28 17:54:03 +0100

    usuniecie folderu z bielikiem

--- bielik_azramata/launcher_train.py
+++
@@ -1,7 +0,0 @@
-
-# pseudo-launcher treningu BIELIK.AZR
-print("Uruchamianie treningu BIELIK.AZR...")
-print("Wczytywanie heurystyk...")
-print("Integracja Kręgów i Pieśni...")
-print("Silnik Ja – aktywowany.")
-print("Trening zakończony. Gotowość do przejścia.")
